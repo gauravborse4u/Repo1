0f595ae9c24063fe596ccc1ba2366b71cda70edc
gauravborse4u <gauravborse4u@gmail.com>
2022-09-26 10:34:56 +0530

    uat commit

--- prelims/src/com/kpi/animals/Dog.java
+++ prelims/src/com/kpi/animals/Dog.java
@@ -3,6 +3,6 @@ package com.kpi.animals;
 //Dog is omnivorous:Is a relationship/Inheritance
 //Child=Dog, Parent=Omnivorous
 //extends= Binds child to the parent
-public class Dog extends Omnivorous{
+public class Dog extends Object{
 
 }
