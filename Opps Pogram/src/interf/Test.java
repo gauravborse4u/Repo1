ca7fb68bfc540f438fca5d9a5f51dc4a146d4b2e
gauravborse4u <gauravborse4u@gmail.com>
2022-09-28 15:39:11 +0530

    Commit for Default Method in interface

--- Opps Pogram/src/interf/Test.java
+++ Opps Pogram/src/interf/Test.java
@@ -2,9 +2,9 @@ package interf;
 
 
 interface I1{
-	void name();
-	default int color() {
-		return 1;
+	abstract void name();
+	default void add(int a, int b) {
+		System.out.println("Addition is :" + (a+b));
 	}
 }
 public class Test implements I1{
@@ -12,14 +12,15 @@ public class Test implements I1{
 	@Override
 	public void name() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("This is an Abstract Method");
 	}
 	
 	public static void main(String args[]) {
 		Test t=new Test();
 		
 		t.name();
-		t.color();
+		t.add(10,40);
+		t.add(70,40);
 	}
 
 }
