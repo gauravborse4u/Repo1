4f62f1f8283d6dfca3228104b3195b2264f2e621
gauravborse4u <gauravborse4u@gmail.com>
2022-10-07 18:03:22 +0530

    Commit for Date

--- kpi-junitdemo/src/main/java/com/Details2.java
+++ kpi-junitdemo/src/main/java/com/Details2.java
@@ -1,7 +1,9 @@
 package com;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 
 public class Details2 {
 
@@ -12,12 +14,16 @@ public class Details2 {
 		
 		LocalDate year = date.plus(8, ChronoUnit.YEARS);
 		System.out.println("Next date in 2030 is  : " + year);
+		
+		LocalDate day=year.with(TemporalAdjusters.next(DayOfWeek.WEDNESDAY));
+		System.out.println("Next Wed of the 2030 : " + day);
 	}
 	
 	public static void main(String a[]) {
 		find();
 		//Output : Current Date is : 2022-10-07
 		//Next date in 2030 is  : 2030-10-07
+		//Day of the Wed : 2030-10-09
 	}
 	
 }
